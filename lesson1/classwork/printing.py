7bd5670305bb3d30c24cf4fa1b25beb3df4a45da
CodeGaming654 <codegaming654@gmail.com>
2026-05-14 02:01:32 +0000

    lesson 1 homework

---
+++ lesson1/classwork/printing.py
@@ -0,0 +1,4 @@
+print("Hello world")
+print("My name is Anish!")
+print('My favorite food is Pizza.')
+print("My favorite animal is cat")
--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -1,24 +1,38 @@
 # Problem 1
 # Create a variable for your lucky number and print it.
 
-
+luckyNumber=7
+print(luckyNumber)
 
 # Problem 2
 # Create a variable for the number of hours you sleep and print it.
 
+sleepHours=11
+print(sleepHours)
 
 
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
 
+fruit="Mango"
+print(fruit + " is my favorite fruit.")
 
 
 # Problem 4
 # Create two variables: one for your city and one for your country.
 # Print them on 2 separate lines.
 
+city="Walka Walka"
+country="Washington"
+print(city)
+print(country)
 
 
 # Problem 5
 # Create 3 variables: your pet's name, its age, and its type.
 # Print them on the same line in a full sentence.
+
+pet="Mango"
+age="1"
+type="parakeet"
+print("My pet's name is " + pet + ", its age is " + age + " and it's a " + type + ".")
