3342a22dc60292d538dc11ce0e5918d030ce3922
CodeGaming654 <codegaming654@gmail.com>
2026-05-17 01:03:48 +0000

    commited

---
+++ lesson2/classwork/casting.py
@@ -0,0 +1,18 @@
+num_str = "42"
+num_int = int(num_str)  # Convert string to integer
+print(num_int + 1)
+
+float_str = "3.14"
+num_float = float(float_str)  # Convert string to float
+print(num_float + 2.19)
+
+int_num = 7
+float_num = float(int_num)  # Convert integer to float
+print(float_num)
+
+float_num2 = 9.99
+int_num2 = int(float_num2)  # Convert float to integer (doesn't round, truncates decimal)
+print(int_num2)
+num = 100
+num_str2 = str(num)  # Convert integer to string
+print(num_str2)
---
+++ lesson2/classwork/math_operators.py
@@ -0,0 +1,27 @@
+print(4 + 5)  # Addition: adds two numbers
+
+print(10 - 5)  # Subtraction: subtracts the second number from the first number
+
+print(6 * 7)  # Multiplication: multiplies two numbers
+
+print(15 / 2)  # Division: divides and gives a float result
+
+print(15 // 2)  # Floor division: divides and rounds down to whole number
+
+print(15 % 2)  # Modulus: gives the remainder
+
+print (2 ** 3) # Exponentiation: first number raised to the power of second number
+
+# You can combine numbers and math operators
+# Python follows the order of operations
+print(2 + 3 * 4)  # prints 14 (3 * 4 = 12, then 2 + 12)
+print((2 + 3) * 4)  # print 20 (2 + 3 = 5, then 5 * 4)
+
+a = 8
+b = 3
+print("a + b = ", a + b)
+print("a * b = ", a * b)
+
+x = int(input("Enter a number: "))
+y = float(input("Enter another number: "))
+print("Sum is", x + y)
